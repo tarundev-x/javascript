df19236489065613fe59d13971beff1fe8cb5afc
tarundev-x <62273965+tarundev-x@users.noreply.github.com>
2021-09-23 00:06:25 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,11 @@
+function food(food)
+{
+console.log("i love to eat "+food);
+}
+function order(quantity,price)
+{
+  console.log("i have ordered "+quantity+"slice");
+  console.log("each slice cost "+price+" $");
+}
+food("pizza");
+order(5,10);
---
+++ loopsstatement.js
@@ -0,0 +1,5 @@
+var x=8;
+for(var i=0;i<x;i++)
+{
+  console.log(i);
+}
